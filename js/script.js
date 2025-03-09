c80efe03f31484a462ec4e9d438c57dfc9b3fac5
PATKORRRR <rochmanfatchur4137@gmail.com>
2025-03-09 20:56:06 +0700

    konfigurasi kedua mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // Ini file javascript
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mysidenav").style.width = "250px";
+    document.getElementById("main-bar").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+    document.getElementById("main-bar").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+  function searchFunction(){
+    openNav();
+    setTimeout(()=>{
+      document.getElementById("search-container").focus();
+    },300);
+  }
